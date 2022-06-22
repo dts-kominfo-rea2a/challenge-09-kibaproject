56f13e81a55a38666ff56ed5b40afbbf6abf348b
unknown <kh.kiba.project@gmail.com>
2022-06-22 19:39:17 +0700

    tantangan ke-9

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
+import "./App.css";
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
+
+  const renderContacts = () => {
+    return contacts.map((contact, index) => (
+      <Contact data={contact} key={index} />
+    ));
+  };
   return (
     <div className="App">
-      
+      <Header />
+      {renderContacts()}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data: { name, phone, email, photo } }) => {
+  return (
+    <div className="contact">
+      <div className="left">
+        <img src={photo} alt={name} />
+      </div>
+      <div className="right">
+        <div>
+          <h2>{name}</h2>
+          <p>{phone}</p>
+          <p>{email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
